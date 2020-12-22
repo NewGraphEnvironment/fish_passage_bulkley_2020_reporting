f62e9beb27d7ca0f539f49762d2d78b7a17277fa
NewGraphEnvironment <al@newgraphenvironment.com>
2020-12-22 12:43:07 -0800

    remove the pdf and insert only html for download.  Ignored it though because it might be too big soon so need to be careful

--- R/run.R
+++ R/run.R
@@ -9,9 +9,11 @@ preview_chapter('0800-appendix-197533.Rmd')
 preview_chapter('0800-appendix-197555.Rmd')
 
 
+##make our printable pdf
+rmarkdown::render_site(output_format = 'pagedown::html_paged', encoding = 'UTF-8')
+
+##copy it to the docs folder so that it can be downloaded
+file.copy('Elk.html', 'docs/Elk.html')
+
 ##make the site
 rmarkdown::render_site(output_format = 'bookdown::gitbook', encoding = 'UTF-8')
-
-
-
-rmarkdown::render_site(output_format = 'pagedown::html_paged', encoding = 'UTF-8')

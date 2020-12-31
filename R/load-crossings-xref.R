9dd7c9864b07adb194f6efd2f7fc48528d81a99b
NewGraphEnvironment <al@newgraphenvironment.com>
2020-12-31 10:28:18 -0800

    add the pscis_phase1b crossings to the xref table

--- R/load-crossings-xref.R
+++ R/load-crossings-xref.R
@@ -19,9 +19,29 @@ xref_pscis_my_crossing_modelled <- dat %>%
   mutate(external_crossing_reference = as.integer(external_crossing_reference)) %>%
   sf::st_drop_geometry()
 
+##I think we need to add our phase1b file in
+##here we find identical columns because we will want to remove them from one of the lists.
+# rows_to_remove <- intersect(xref_pscis_my_crossing_modelled$external_crossing_reference, pscis$my_crossing_reference)
+rows_to_keep <- setdiff(pscis$my_crossing_reference, xref_pscis_my_crossing_modelled$external_crossing_reference)  ##make sure to keep the joining column
+
+pscis_rows_to_add <- pscis %>%
+  filter(my_crossing_reference %in% rows_to_keep) %>%
+  select(my_crossing_reference)
+
+##add the new info to the xref
+xref_pscis_my_crossing_modelled <- bind_rows(
+  xref_pscis_my_crossing_modelled,
+  pscis_rows_to_add
+) %>%
+  mutate(my_crossing_reference = as.integer(my_crossing_reference)) %>%
+  mutate(external_crossing_reference = case_when(
+    is.na(external_crossing_reference) ~ my_crossing_reference,
+    T ~ external_crossing_reference
+  )) %>%
+  select(-my_crossing_reference)
 
 #get our modelled crossing info
-xref_bcfishpass <- readr::read_csv(file = paste0(getwd(), '/data/bcfishpass.csv')) %>%
+xref_bcfishpass <- readr::read_csv(file = paste0(getwd(), '/data/bcfishpass-phase2.csv')) %>%
   select(misc_point_id, crossing_id, my_crossing_reference) %>%
   mutate(my_crossing_reference = as.integer(my_crossing_reference))
 

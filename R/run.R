e3370c29327d7790ae80b3b9304fb2a3b69fa41d
NewGraphEnvironment <al@newgraphenvironment.com>
2021-01-04 14:01:34 -0800

    play by play for biuld of both formats

--- R/run.R
+++ R/run.R
@@ -1,6 +1,7 @@
 ##this is for as we work through
 # preview_chapter('index.Rmd')
 # preview_chapter('0300-method.Rmd')
+preview_chapter('0400-results.Rmd')
 # preview_chapter('0200-background.Rmd')
 # preview_chapter('0800-appendix-050155.Rmd')
 # preview_chapter('0800-appendix-050181.Rmd') ##nupku
@@ -16,8 +17,16 @@
 
 ##for a prod build
 
+
+##change your VErsion #
+
 ##move the phase 1 appendix out of the main directory to a backup file
 file.rename('0600-appendix.Rmd', 'data/0600-appendix.Rmd')
+
+##go to the index.Rmd doc and turn
+#html_on <- FALSE and change
+#font_set <- 9
+#photo_width <- "80%"
 
 ##   then make our printable pdf
 rmarkdown::render_site(output_format = 'pagedown::html_paged', encoding = 'UTF-8')
@@ -28,7 +37,10 @@ file.rename('Elk.html', 'docs/Elk.html')
 ##move the phase 1 appendix back to main directory
 file.rename('data/0600-appendix.Rmd', '0600-appendix.Rmd')
 
+##go to the index.Rmd doc and turn
+#html_on <- TRUE and change
+#font_set <- 11
+#photo_width <- "100%"
+
 ##  make the site
 rmarkdown::render_site(output_format = 'bookdown::gitbook', encoding = 'UTF-8')
-
-

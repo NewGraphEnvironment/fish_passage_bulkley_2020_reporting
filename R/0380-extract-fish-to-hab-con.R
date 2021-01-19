94fe58bb7cb155b98485c3ff0ba53c2caa4be28c
NewGraphEnvironment <al@newgraphenvironment.com>
2021-01-19 12:09:48 -0800

    rename file along with multiple changes to get it to biuld the coll summary properly

--- R/0380-extract-fish-to-hab-con.R
+++ R/0380-extract-fish-to-hab-con.R
@@ -1,8 +1,6 @@
-source('R/packages.R')
-source('R/functions.R')
-source('R/tables-phase2.R')
-
-
+# source('R/packages.R')
+# source('R/functions.R')
+source('R/0320-tables-phase2.R')
 
 
 ##proces the fsih data for upstream and downstream of crossings
@@ -25,42 +23,43 @@ hab_fish_indiv_prep3 <- left_join(
   select(hab_fish_codes, common_name:species_code),
   by = c('species' = 'common_name')
 ) %>%
-  dplyr::select(reference_number, alias_local_name, site_number, species_code, length_mm)
+  dplyr::select(reference_number, alias_local_name, site_number, method_number, species_code, length_mm) ##added method #
 
 
 ##we need the size of the sites too
 
 ####workflow is a bit weird because we need to input NFC sites and the size of the sites
 ##or else we don't know about them in the summary.
-hab_fish_collect_prep <- habitat_confirmations %>%
-  purrr::pluck("step_2_fish_coll_data") %>%
-  dplyr::filter(!is.na(site_number)) %>%
-  # select(-gazetted_name:-site_number) %>%
-  dplyr::distinct(reference_number, .keep_all = T)
-
+# hab_fish_collect_prep <- habitat_confirmations %>%
+#   purrr::pluck("step_2_fish_coll_data") %>%
+#   dplyr::filter(!is.na(site_number)) %>%
+#   # select(-gazetted_name:-site_number) %>%
+#   dplyr::distinct(reference_number, method_number, .keep_all = T) ##added method #
 
 ##
 hab_fish_indiv <- left_join(
    select(hab_fish_collect_prep,
           reference_number,
           local_name,
-          site_number:model
+          site_number:model, date_in:time_out ##added date_in:time_out
           ),
-  select(hab_fish_indiv_prep3, reference_number,
+  select(hab_fish_indiv_prep3,
+         reference_number,
+         method_number, ##added method #
          # alias_local_name,
          species_code, length_mm),
-  by = 'reference_number'
+  by = c('reference_number', 'method_number') #added method #
 ) %>%
   mutate(species_code = as.character(species_code)) %>%
   mutate(species_code = case_when(
     is.na(species_code) ~ 'NFC',
     T ~ species_code)
   ) %>%
-  mutate(species_code = as.factor(species_code))   %>%
+  mutate(species_code = as.factor(species_code))  %>%
   mutate(life_stage = case_when(  ##this section comes from the histogram below - we include here so we don't need to remake the df
-    length_mm <= 60 ~ 'fry',
-    length_mm > 60 & length_mm <= 110 ~ 'parr',
-    length_mm > 110 & length_mm <= 140 ~ 'juvenile',
+    length_mm <= 70 ~ 'fry',
+    length_mm > 70 & length_mm <= 100 ~ 'parr',
+    length_mm > 100 & length_mm <= 140 ~ 'juvenile',
     length_mm > 140 ~ 'adult',
     T ~ NA_character_
   )) %>%
@@ -73,7 +72,7 @@ hab_fish_indiv <- left_join(
   mutate(site_id = paste0(site, '_', location))
 
 tab_fish_summary <- hab_fish_indiv %>%
-  group_by(site_id, species_code) %>%
+  group_by(site_id, sampling_method, species_code) %>% ##added sampling method!
   summarise(count_fish = n())
 
 
@@ -81,34 +80,36 @@ tab_fish_summary <- hab_fish_indiv %>%
 ###------from duncan_fish_plots_20200210
 
 ####----------fish length-----------
-fish_wct <- hab_fish_indiv %>%  filter(species_code == "WCT")
-fish_eb <-  hab_fish_indiv %>% filter(species_code != "EB")
+fish <- hab_fish_indiv
+  # filter(species_code == "CO")
+# fish_eb <-  hab_fish_indiv %>% filter(species_code != "EB")
 
-bin_1 <- floor(min(fish_wct$length_mm, na.rm = TRUE)/5)*5
-bin_n <- ceiling(max(fish_wct$length_mm, na.rm = TRUE)/5)*5
+bin_1 <- floor(min(fish$length_mm, na.rm = TRUE)/5)*5
+bin_n <- ceiling(max(fish$length_mm, na.rm = TRUE)/5)*5
 bins <- seq(bin_1,bin_n, by = 5)
 
-plot_fish_hist_wct <- ggplot(fish_wct, aes(x=length_mm
+plot_fish_hist <- ggplot(fish %>% filter(!species_code %in% c("MW",'SU','NFC', 'DV')),
+                         aes(x=length_mm
                                           # fill=alias_local_name
                                          # color = alias_local_name
                                          )) +
-  geom_histogram(breaks = bins, alpha=0.5,
+  geom_histogram(breaks = bins, alpha=0.75,
                  position="identity", size = 0.75)+
-  labs(x = "Fork Length (mm)", y = "Count WCT (#)") +
-  # facet_wrap(~alias_local_name)+
-  scale_color_grey() +
-  scale_fill_grey() +
+  labs(x = "Fork Length (mm)", y = "Count (#)") +
+  facet_wrap(~species_code)+
+  # scale_color_grey() +
+  # scale_fill_grey() +
   theme_bw(base_size = 8)+
   scale_x_continuous(breaks = bins[seq(1, length(bins), by = 2)])+
   # scale_color_manual(values=c("grey90", "grey60", "grey30", "grey0"))+
-  # theme(axis.text.x = element_text(angle = 45, hjust = 1))+
-  geom_histogram(aes(y=..density..), breaks = bins, alpha=0.5,
-                 position="identity", size = 0.75)
-plot_fish_hist_wct
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
+  # geom_histogram(aes(y=..density..), breaks = bins, alpha=1,
+  #                position="identity", size = 0.75)
+plot_fish_hist
 
 
-ggsave(plot = plot_fish_hist_wct, file="./fig/fish_histogram.png",
-       h=3.4, w=5.11, units="in", dpi=300)
+# ggsave(plot = plot_fish_hist, file="./fig/fish_histogram.png",
+#        h=3.4, w=5.11, units="in", dpi=300)
 
 ####-----------summary tables for input to spreadsheet----------------------
 hab_fish_input_prep <- hab_fish_indiv %>%
@@ -127,7 +128,7 @@ hab_fish_input <- left_join(
   by = 'species_code'
 ) %>%
   ungroup() %>%
-  select(reference_number:model, common_name, stage = life_stage, total_number = n,
+  select(reference_number:time_out, common_name, stage = life_stage, total_number = n,
          min, max) %>%
   mutate(total_number = case_when(
     common_name == 'No Fish Caught' ~ NA_integer_,
@@ -136,17 +137,15 @@ hab_fish_input <- left_join(
   # janitor::adorn_totals()   ##use this to ensure you have the same number of fish in the summary as the individual fish sheet
 
 ##burn to a csv so you can cut and paste into your fish submission
-# hab_fish_input %>%
-#   readr::write_csv(file = paste0(getwd(), '/data/raw_input/habitat_confirmation_fish_summary.csv'))
-
-##this is the second time we did it - changed to habitat_confirmation_fish_summary2.csv'
 hab_fish_input %>%
-  readr::write_csv(file = paste0(getwd(), '/data/raw_input/habitat_confirmation_fish_summary2.csv'))
+  readr::write_csv(file = paste0(getwd(), '/data/extracted_inputs/hab_con_fish_summary.csv'),
+                   na = "")
 
 
 ######----------------density plots--------------------------
 
 hab_fish_dens <- hab_fish_indiv %>%
+  filter(sampling_method == 'electrofishing') %>% ##added this since we now have mt data as well!!
   mutate(area = round(ef_length_m * ef_width_m),0) %>%
   group_by(local_name, site_number, ef_length_m, ef_width_m, ef_seconds, area, species_code, life_stage) %>%
   summarise(fish_total = length(life_stage)) %>%

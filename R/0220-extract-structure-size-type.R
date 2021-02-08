35f3371141fd9ae7ee627d65ff6a0ec71719b3e9
NewGraphEnvironment <al@newgraphenvironment.com>
2021-02-08 08:46:17 -0800

    use 6m as max channel width for 10m span and make spans always only 4m larger than channel width if fill is not an issue

--- R/0220-extract-structure-size-type.R
+++ R/0220-extract-structure-size-type.R
@@ -26,7 +26,7 @@ fill_dpth <- 3
 brdg_wdth <- 10
 
 ##% bigger than the channel that the bridge should be
-chn_wdth_max <- 5
+chn_wdth_max <- 6 ##changed this from 5
 
 
 
@@ -45,19 +45,28 @@ tab_backwater <- pscis %>%  ##changed this to pscis2!
 
 ##need to tweak the span so that we add 4 to the width if it is over 6m...
 str_type <- pscis %>%
-  select(column_num, pscis_crossing_id, my_crossing_reference, downstream_channel_width_meters, fill_depth_meters) %>%
+  select(column_num, pscis_crossing_id, my_crossing_reference, barrier_result, downstream_channel_width_meters, fill_depth_meters) %>%
   mutate(fill_dpth_over = fill_depth_meters - fill_dpth_mult) %>%
-  mutate(span_input = case_when(downstream_channel_width_meters >= 2 ~ brdg_wdth, T ~ NA_real_))  %>%
+  mutate(crossing_fix = case_when((barrier_result == 'Barrier' | barrier_result == 'Potential')
+                                & downstream_channel_width_meters >= 2 ~ 'Replace with New Open Bottom Structure',
+                                barrier_result == 'Passable' | barrier_result == 'Unknown' ~ NA_character_,
+                                T ~ 'Replace Structure with Streambed Simulation CBS'))  %>%
+  mutate(span_input = case_when((barrier_result == 'Barrier' | barrier_result == 'Potential')
+                                & downstream_channel_width_meters >= 2 ~ brdg_wdth,
+                                barrier_result == 'Passable' | barrier_result == 'Unknown' ~ NA_real_,
+                                T ~ 3))  %>%
   mutate(span_input = case_when(fill_dpth_over > 0 ~
-                                      (brdg_wdth + fill_dpth_mult * fill_dpth_over),  ##1m more fill = 3 m more bridge
-                                    T ~ span_input)) %>%
-  mutate(span_input = case_when(downstream_channel_width_meters > 5 ~
-                                      (downstream_channel_width_meters - chn_wdth_max) * 2 + span_input,  ##for every m bigger than a 5 m channel add that much to each side in terms of span
-                                    T ~ span_input)) %>%
+                                  (brdg_wdth + fill_dpth_mult * fill_dpth_over),  ##1m more fill = 3 m more bridge
+                                T ~ span_input)) %>%
+  mutate(span_input = case_when(span_input < (downstream_channel_width_meters + 4) & ##span not need be extended if already 4m bigger than channel width
+                                  downstream_channel_width_meters > chn_wdth_max ~
+                                  (downstream_channel_width_meters - chn_wdth_max) + span_input,  ##for every m bigger than a 5 m channel add that much to each side in terms of span
+                                T ~ span_input)) %>%
+  mutate(span_input = round(span_input, 1)) %>%
   arrange(column_num)
 
 ##burn to a csv so you can copy and paste into spreadsheet
 
-# str_type %>% readr::write_csv(file = paste0(getwd(), '/data/raw_input/pscis1_str_type.csv'))
-# str_type %>% readr::write_csv(file = paste0(getwd(), '/data/raw_input/pscis2_str_type.csv'))
-str_type %>% readr::write_csv(file = paste0(getwd(), '/data/raw_input/pscis_reassessments_str_type.csv'))
+# str_type %>% readr::write_csv(file = paste0(getwd(), '/data/extracted_inputs/pscis1_str_type.csv'))
+# str_type %>% readr::write_csv(file = paste0(getwd(), '/data/extracted_inputs/pscis2_str_type.csv'))
+str_type %>% readr::write_csv(file = paste0(getwd(), '/data/extracted_inputs/pscis_reassessments_str_type.csv'))

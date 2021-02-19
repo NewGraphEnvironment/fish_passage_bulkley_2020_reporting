46de60dbad800d9d48c1688544815f755a2d7ee4
NewGraphEnvironment <al@newgraphenvironment.com>
2021-02-18 20:27:41 -0800

    add html version

--- R/0405-report-html-summary-tables.R
+++ R/0405-report-html-summary-tables.R
@@ -12,40 +12,35 @@ print_tab_summary_png <- function(site){
   # kableExtra::scroll_box(width = "100%", height = "500px") ##not scrolling to simplify our pagedown output
 }
 
-# print_tab_summary_phase1_png <- function(site){
-#   make_tab_summary(df = pscis_phase1_reassessments %>% filter(pscis_crossing_id == site)) %>%
-#     kable(booktabs = T) %>%    #
-#     kableExtra::add_footnote(label = paste0('Comments: ', pscis_phase1_reassessments %>% filter(pscis_crossing_id == site) %>% ##might be my_crossing_refe
-#                                               pull(assessment_comment)), notation = 'none') %>% #this grabs the comments out
-#     # kableExtra::add_footnote(label = paste0('Photos: From top left clockwise: Road/Site Card, Barrel, Outlet, Downstream, Upstream, Inlet.',
-#     #                                         paste0('![](data/photos/', site, '/crossing_all.JPG)')), notation = 'none', escape = F) %>%
-#     kableExtra::kable_styling(c("condensed"), full_width = T, font_size = 11) %>%
-#     kableExtra::save_kable(paste0("fig/sum/", site, ".png"))
-#   # kableExtra::scroll_box(width = "100%", height = "500px") ##not scrolling to simplify our pagedown output
-# }
-#
-#
-#
-# ##summary table
-# print_tab_summary_phase2_png <- function(site){
-#   make_tab_summary(df = pscis_phase2 %>% filter(pscis_crossing_id == site)) %>%
-#     kable(booktabs = T) %>%    #
-#     kableExtra::add_footnote(label = paste0('Comments: ', pscis_phase2 %>% filter(pscis_crossing_id == site) %>%
-#                                               pull(assessment_comment)), notation = 'none') %>% #this grabs the comments out
-#     # kableExtra::add_footnote(label = paste0('Photos: From top left clockwise: Road/Site Card, Barrel, Outlet, Downstream, Upstream, Inlet.',
-#     #                                         paste0('![](data/photos/', site, '/crossing_all.JPG)')), notation = 'none', escape = F) %>%
-#     kableExtra::kable_styling(c("condensed"), full_width = T, font_size = 11) %>%
-#     kableExtra::save_kable(paste0("fig/sum/", site, ".png"))
-#   # kableExtra::scroll_box(width = "100%", height = "500px") ##not scrolling to simplify our pagedown output
-# }
+print_tab_summary_html <- function(site){
+  make_tab_summary(df = pscis_all %>% filter(pscis_crossing_id == site)) %>% ##%>% sf::st_drop_geometry()
+    kable(booktabs = T) %>%    #
+    kableExtra::add_footnote(label = paste0('Comments: ', pscis_all %>% filter(pscis_crossing_id == site) %>%
+                                              distinct(pscis_crossing_id, .keep_all = T) %>% ##might be my_crossing_refe
+                                              pull(assessment_comment)), notation = 'none') %>% #this grabs the comments out
+    # kableExtra::add_footnote(label = paste0('Photos: From top left clockwise: Road/Site Card, Barrel, Outlet, Downstream, Upstream, Inlet.',
+    #                                         paste0('![](data/photos/', site, '/crossing_all.JPG)')),
+    #                          notation = 'none',
+    #                          escape = T) %>%
+    kableExtra::kable_styling(c("condensed"), full_width = T, font_size = 18, html_font = 'helvetica') %>%
+    # cat(., file = paste0("docs/sum/", site, ".html")) %>%
+    readr::write_file(., file = paste0("docs/sum/", site, ".html"))
+  # kableExtra::scroll_box(width = "100%", height = "500px") ##not scrolling to simplify our pagedown output
+}
 
 
 # print_tab_summary_phase1_png(site = '4600005')
-sites <- pscis_all %>% pull(pscis_crossing_id)
+sites <- pscis_all %>%
+  # slice(1:2) %>%
+  pull(pscis_crossing_id)
 
+# sites %>%
+#   map(print_tab_summary_png)
+
+
+##we are going to use this one now
 sites %>%
-  map(print_tab_summary_png)
-
+  map(print_tab_summary_html)
 
 # sites_phase1 <- pscis_phase1_reassessments %>% pull(amalgamated_crossing_id)
 #

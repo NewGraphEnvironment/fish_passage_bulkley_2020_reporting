b1cbc6a798278a126d51665339303e8d1e2cc300
NewGraphEnvironment <al@newgraphenvironment.com>
2021-03-01 12:27:23 -0800

    add bib cleaner - not used though

--- R/run.R
+++ R/run.R
@@ -65,6 +65,15 @@ file.rename('data/0600-appendix.Rmd', '0600-appendix.Rmd')
 #################################################################################################
 
 
+##this is how we clean up our bib file.  We need to find a way to add together the packages.bib file with the book.bib file first though.
+# citr::tidy_bib_file(
+#   rmd_file = "Elk-River-Fish-Passage-2020.Rmd",
+#   messy_bibliography = 'book.bib',
+#   file = 'book_tidy.bib')
+
+##we also need to change all the date header to year in the bib file so that it can be run by our pdf maker
+##i did this by hand last time but would be good to automate!!!
+
 ##  make the site
 source('R/photos-extract-metadata.R')
 rmarkdown::render_site(output_format = 'bookdown::gitbook', encoding = 'UTF-8')

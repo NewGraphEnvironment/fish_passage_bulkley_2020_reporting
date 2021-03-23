fcf2aa496adceb4a5586c85c8f084484ef271f8a
NewGraphEnvironment <al@newgraphenvironment.com>
2021-03-23 11:33:00 -0700

    reference input site into bcfp summary tables sub-function

--- R/functions.R
+++ R/functions.R
@@ -459,7 +459,7 @@ make_tab_summary_bcfp <- function(dat = bcfishpass_all,
 }
 
 print_tab_summary_bcfp <- function(site = my_site, font = 11, ...){
-  make_tab_summary_bcfp() %>%
+  make_tab_summary_bcfp(site = site) %>%
     kable(caption = paste0('Summary of fish habitat modelling for PSCIS crossing ', site, '.'), booktabs = T) %>%    #
     kableExtra::add_footnote('Model data is preliminary and subject to adjustments including incorperation of area based estimates.', notation = 'symbol') %>%
     kableExtra::kable_styling(c("condensed"), full_width = T, font_size = font)

d5a7993f08d87a0c3fd0dcee5a2b50c65ca609c4
NewGraphEnvironment <al@newgraphenvironment.com>
2021-04-13 14:07:39 -0700

    updated thresholds

--- R/0291-extract-bcfishpass-spawn-rear-model.R
+++ R/0291-extract-bcfishpass-spawn-rear-model.R
@@ -1,6 +1,8 @@
 ##grab the bcfishpass spawning and rearing table and put in the database so it can be used to populate the methods and tie to the references table
 
-urlfile="https://github.com/smnorris/bcfishpass/raw/main/02_model/data/model_spawning_rearing_habitat.csv"
+# urlfile="https://github.com/smnorris/bcfishpass/raw/main/02_model/data/model_spawning_rearing_habitat.csv"
+urlfile="https://raw.githubusercontent.com/NewGraphEnvironment/bcfishpass/newgraph/02_model/data/model_spawning_rearing_habitat.csv"
+
 
 bcfishpass_spawn_rear_model <- read_csv(url(urlfile))
 
@@ -8,7 +10,10 @@ bcfishpass_spawn_rear_model <- read_csv(url(urlfile))
 ##burn it to the database so we have a stable version for reporting
 conn <- rws_connect("data/bcfishpass.sqlite")
 rws_list_tables(conn)
-# rws_drop_table("bcfishpass_spawn_rear_model", conn = conn)
+archive <- readwritesqlite::rws_read_table("bcfishpass_spawn_rear_model", conn = conn)
+rws_write(archive, exists = F, delete = TRUE,
+          conn = conn, x_name = paste0("bcfishpass_spawn_rear_model_archive_", format(Sys.time(), "%Y-%m-%d-%H%m")))
+rws_drop_table("bcfishpass_spawn_rear_model", conn = conn)
 rws_write(bcfishpass_spawn_rear_model, exists = F, delete = TRUE,
           conn = conn, x_name = "bcfishpass_spawn_rear_model")
 rws_list_tables(conn)

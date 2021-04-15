e28baa907bf54e6efe383e6f354cfe311e41178f
NewGraphEnvironment <al@newgraphenvironment.com>
2021-04-14 23:59:05 -0700

    combine sern and hctf photos

--- R/photos-build-logo-combined.R
+++ R/photos-build-logo-combined.R
@@ -24,4 +24,14 @@ image_read('data/photos/124487/IMG_8245_k_d1_.JPG') %>%
   image_rotate(180) %>%
   image_write(path = "data/photos/124487/IMG_8245_k_d1_rotated.JPG", format = 'jpg')
 
+##resize hctf logo
+image_read("fig/hctf_large.jpg") %>%
+  image_convert("png") %>%
+  image_scale("200") %>%
+  image_write(path = "fig/logo_hctf.png", format = 'png')
 
+##combine newgraph and hctf logos
+images_to_combine <- paste0(getwd(), c('/fig/logo_hctf.png','/fig/logo_html3.png')) %>%
+  image_read() %>%
+  image_append(stack = T)  %>%  ##half the original height
+  image_write(path = paste0(getwd(), '/fig/logo_hctf_newg.png'), format = 'png')
